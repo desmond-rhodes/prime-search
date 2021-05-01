319547d1cfbbc81f82db5e74761ce021d257c259
Desmond Rhodes <desmond.rhodes@outlook.com>
2021-05-01 10:00:43 +0700

    Take search range from argument

--- prime.py
+++ prime.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3.8
 
+import sys
+
+if len(sys.argv) != 2:
+	print('usage:')
+	print('    (ceiling)')
+	print('        Find primes less than ceiling.')
+	sys.exit(-1)
+
+try:
+	ceiling = int(sys.argv[1])
+except:
+	print('Fail to parse ceiling ({}) as integer.'.format(sys.argv[1]))
+	sys.exit(-2)
+
 prime = []
 
 # range(start, end) does not include end
-for i in range(2,101):
+for i in range(2, ceiling + 1):
 	for j in prime:
 		if i % j == 0:
 			break
@@ -14,3 +28,4 @@ for i in range(2,101):
 print(prime)
 
 print('Hello, world!')
+sys.exit(0)
