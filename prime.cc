2990c5d3a814a325086c42ee1150902e2831bbf2
Desmond Rhodes <desmond.rhodes@outlook.com>
2021-05-01 20:58:22 +0700

    Odd numbers and square root

--- prime.cc
+++ prime.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <chrono>
+#include <cmath>
 
 int main(int argc, char* argv[]) {
 	std::ios_base::sync_with_stdio(false);
@@ -28,12 +29,18 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	if (ceiling < 2) {
+		std::cerr << "Minimum ceiling value is 2.\n";
+		return -1;
+	}
+
 	std::vector<unsigned long long> prime {};
 
 	auto time_s {std::chrono::steady_clock::now()};
 
-	for (unsigned long long i {2}; i <= ceiling; ++i) {
-		for (size_t j {0}; j < prime.size(); ++j)
+	for (unsigned long long i {3}; i <= ceiling; i+=2) {
+		auto root {std::sqrt(i)};
+		for (size_t j {0}; j < prime.size() && root >= prime[j]; ++j)
 			if (i % prime[j] == 0)
 				goto skip;
 		prime.push_back(i);
@@ -43,11 +50,12 @@ int main(int argc, char* argv[]) {
 	auto time_e {std::chrono::steady_clock::now()};
 	std::chrono::duration<double, std::milli> time_d {time_e-time_s};
 
+	std::cout << '2';
 	for (auto i : prime)
-		std::cout << i << ' ';
+		std::cout << ' ' << i;
 	std::cout << '\n';
 
-	std::cout << "Number of, primes less than or equal to " << ceiling << ", found: " << prime.size() << '\n';
+	std::cout << "Number of, primes less than or equal to " << ceiling << ", found: " << prime.size()+1 << '\n';
 	std::cout << "Time elapsed: " << time_d.count() << "ms\n";
 
 	return 0;
