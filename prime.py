d07a07153ba33b9a1725bc3c0d7446ab4f030c89
Desmond Rhodes <desmond.rhodes@outlook.com>
2021-05-01 07:03:55 +0700

    Find primes less than 100

--- prime.py
+++ prime.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3.8
 
+prime = []
+
+# range(start, end) does not include end
+for i in range(2,101):
+	for j in prime:
+		if i % j == 0:
+			break
+	# else after for loop don't execute when exit with break
+	else:
+		prime.append(i)
+
+print(prime)
 
 print('Hello, world!')
