c002f2f6c651e2e43b21df3d8ab3e6631e122206
Desmond Rhodes <desmond.rhodes@outlook.com>
2021-05-02 11:39:20 +0700

    Remove 3

--- prime.cc
+++ prime.cc
@@ -28,25 +28,23 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	if (ceiling < 3) {
-		std::cerr << "Minimum ceiling value is 3.\n";
+	if (ceiling < 10) {
+		std::cerr << "Minimum ceiling value is 10.\n";
 		return -1;
 	}
 
-	std::vector<unsigned long long> prime {3};
+	std::vector<unsigned long long> prime {5, 7};
 	size_t r {0};
 	auto square {prime[r]*prime[r]};
 
-	ceiling += 2;
-
 	auto time_s {std::chrono::steady_clock::now()};
 
-	for (unsigned long long i {7}; i <= ceiling; i+=6) {
-		if (square <= i) {
-			++r;
-			square = prime[r]*prime[r];
-		}
-		for (auto i : {i-2, i}) {
+	for (unsigned long long i {11}; i <= ceiling; i+=6) {
+		for (auto i : {i, i+2}) {
+			if (square <= i) {
+				++r;
+				square = prime[r]*prime[r];
+			}
 			for (size_t j {0}; j < r; ++j)
 				if (i % prime[j] == 0)
 					goto skip;
@@ -58,16 +56,15 @@ int main(int argc, char* argv[]) {
 	auto time_e {std::chrono::steady_clock::now()};
 	std::chrono::duration<double, std::milli> time_d {time_e-time_s};
 
-	ceiling -= 2;
 	if (prime.back() > ceiling)
 		prime.pop_back();
 
-	std::cout << "[2";
+	std::cout << "[2, 3";
 	for (auto i : prime)
 		std::cout << ", " << i;
 	std::cout << "]\n";
 
-	std::cout << "Number of, primes less than or equal to " << ceiling << ", found: " << prime.size()+1 << '\n';
+	std::cout << "Number of, primes less than or equal to " << ceiling << ", found: " << prime.size()+2 << '\n';
 	std::cout << "Time elapsed: " << time_d.count() << "ms\n";
 
 	return 0;
